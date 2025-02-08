eb2b2d9e0ce8114e1b2673f31130dec3832586b9
David Valdman <dmvaldman@gmail.com>
2025-02-07 21:35:32 -0800

    Gem

--- _plugins/highlight_block.rb
+++ _plugins/highlight_block.rb
@@ -6,38 +6,8 @@ module Kramdown
     class MyParser < Kramdown::Parser::Kramdown
       def initialize(source, options)
         super
-        @block_parsers.unshift(:highlight_block, :chat_send_block, :chat_recv_block)
-        @span_parsers.unshift(:highlight_span)
+        @block_parsers.unshift(:chat_send_block, :chat_recv_block)
       end
-
-      # Highlight block parser (existing code)
-      HIGHLIGHT_START = /^<highlight>\s*?\n/
-      HIGHLIGHT_END = /^<\/highlight>\s*?\n/
-      HIGHLIGHT_SPAN = /<highlight>(?<content>.*?)<\/highlight>/m
-
-      def parse_highlight_span
-        # Use scan—which advances @src automatically—rather than manually moving @src.pos.
-        if @src.scan(HIGHLIGHT_SPAN)
-          content = @src[:content]
-          # Create a span element with the highlight class.
-          el = new_span_el(:html_element, 'span', {'class' => 'highlight'})
-          parse_spans(el, content)
-          @tree.children << el
-        end
-      end
-      define_parser(:highlight_span, HIGHLIGHT_SPAN)
-
-      def parse_highlight_block
-        @src.pos += @src.matched_size
-        content = @src.scan_until(HIGHLIGHT_END)
-        if content
-          content = content.sub(HIGHLIGHT_END, '')
-          block = new_block_el(:html_element, 'div', {'class' => 'highlight'})
-          parse_blocks(block, content)
-          @tree.children << block
-        end
-      end
-      define_parser(:highlight_block, HIGHLIGHT_START)
 
       # Chat send block parser
       CHAT_SEND_START = /^<chat_send>\s*?\n/
