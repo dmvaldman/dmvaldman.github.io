ee557ba5fd703a77c7f3144182c3f24539861fab
David Valdman <dmvaldman@gmail.com>
2026-07-02 16:25:41 -0700

    Fix latex

--- assets/js/plain-dollars.js
+++ assets/js/plain-dollars.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
+  var plainDollarAmount = /\$([0-9][0-9,]*(?:\.[0-9]+)?)(?=\s|[.,;:!?)]|$)/;
+  var plainDollarAmounts = /\$([0-9][0-9,]*(?:\.[0-9]+)?)(?=\s|[.,;:!?)]|$)/g;
   var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, {
     acceptNode: function (node) {
       var parent = node.parentNode;
       if (!parent || /^(SCRIPT|STYLE|TEXTAREA|CODE|PRE)$/i.test(parent.nodeName)) {
         return NodeFilter.FILTER_REJECT;
       }
-      return /\$[0-9]/.test(node.nodeValue) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
+      return plainDollarAmount.test(node.nodeValue) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
     }
   });
 
   var nodes = [];
   while (walker.nextNode()) nodes.push(walker.currentNode);
   nodes.forEach(function (node) {
-    node.nodeValue = node.nodeValue.replace(/\$([0-9])/g, '\\$$1');
+    node.nodeValue = node.nodeValue.replace(plainDollarAmounts, function (_match, amount) {
+      return '\\$' + amount;
+    });
   });
 });
